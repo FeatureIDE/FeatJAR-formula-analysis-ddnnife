e567b00eb612e898ab6414fb0bd54958305f610b
Sebastian Krieter <sebastian.krieter@uni-ulm.de>
2023-02-23 16:51:19 +0100

    Updates licence header

--- src/main/java/de/featjar/analysis/ddnnife/DdnnifeAnalysis.java
+++ src/main/java/de/featjar/analysis/ddnnife/DdnnifeAnalysis.java
@@ -1,20 +1,20 @@
 /*
- * Copyright (C) 2022 Sebastian Krieter
+ * Copyright (C) 2023 Sebastian Krieter
  *
- * This file is part of formula-analysis-sharpsat.
+ * This file is part of formula-analysis-ddnnife.
  *
- * formula-analysis-sharpsat is free software: you can redistribute it and/or modify it
+ * formula-analysis-ddnnife is free software: you can redistribute it and/or modify it
  * under the terms of the GNU Lesser General Public License as published by
  * the Free Software Foundation, either version 3.0 of the License,
  * or (at your option) any later version.
  *
- * formula-analysis-sharpsat is distributed in the hope that it will be useful,
+ * formula-analysis-ddnnife is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
  * See the GNU Lesser General Public License for more details.
  *
  * You should have received a copy of the GNU Lesser General Public License
- * along with formula-analysis-sharpsat. If not, see <https://www.gnu.org/licenses/>.
+ * along with formula-analysis-ddnnife. If not, see <https://www.gnu.org/licenses/>.
  *
  * See <https://github.com/FeatJAR/formula-analysis-sharpsat> for further information.
  */
